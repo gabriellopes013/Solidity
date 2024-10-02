13f5ed3b2494cbc9fc8898be934dd4bf0244a00b
gabriellopes013 <gabrielalmeida99@gmail.com>
2024-10-01 23:43:37 -0300

    Add project

---
+++ contracts/TesteVariavel.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract TesteVariavel{
+    string internal texto1 = "Ola mundo"; // Sem nada pode ser internal
+    string public texto2;
+    string private texto3;
+
+    //[TIPO] [VISIBILIDADE] [NOME]
+}
